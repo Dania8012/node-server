3300ce51a4d60979e696dfd348dab48e2024cdbd
Dania Alrefai <daniaalrefai@BGLTM0012.local>
2022-06-22 19:53:08 +0400

    New version v0.01

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log("Sending new logs");
-  res.send("Hello World! v5");
+  res.send("Hello World! v0.01");
 });
 
 app.listen(port, () => {
